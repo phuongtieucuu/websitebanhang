05582da89deb87ae08625525aa1f3b829ce0bd69
Thaihuy <hamo.thaihuy@gmail.com>
2022-08-21 15:44:38 +0700

    C0004

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const port =  3000
-const handlebars = require('express-handlebars')
+// const handlebars = require('express-handlebars')
 // const path = require('path')
-const router = require('./routes/index')
+// const router = require('./routes/index')
 const mongoose = require('mongoose')
 // const methodOverride = require('method-override')
 // var morgan = require('morgan')
@@ -12,7 +12,7 @@ const mongoose = require('mongoose')
 // const expressValidator = require('express-validator')
 // const cookieParser = require('cookie-parser')
 // const session = require('express-session');
-// const Category = require('./app/models/category')
+const Category = require('./app/models/category')
 // const fileUpload = require('express-fileupload')
 // const {ObtoOb,ArtoOb} = require('./until/mongooes')
 // const passport = require('passport')
@@ -31,9 +31,9 @@ mongoose
 // app.use(cookieParser());
 // app.use(fileUpload())
 // app.use(session({secret: 'mk', saveUninitialized: true, resave: false,cookie: { secure: false }}))
-app.engine('.hbs', handlebars.engine({extname: '.hbs'}))
-app.set('view engine', '.hbs');
-app.set('views', './src/resources/views')
+// app.engine('.hbs', handlebars.engine({extname: '.hbs'}))
+// app.set('view engine', '.hbs');
+// app.set('views', './src/resources/views')
 // app.use(passport.initialize())
 // app.use(passport.session())
 // require('./until/passport')(passport)
@@ -45,7 +45,12 @@ app.set('views', './src/resources/views')
 //     app.locals.category = ArtoOb(data)
 //   }
 // })
-router(app)
+// router(app)
+app.get('/',(req, res, next)=>{
+  Category.find({},(err,data)=>{
+    res.json(data)
+  })
+})
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}`)
 })
