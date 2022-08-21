4412b269d62023092bc8076bc1100c8a8449f7de
Thaihuy <hamo.thaihuy@gmail.com>
2022-08-21 16:27:42 +0700

    C0013

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,11 +6,7 @@ var router = express.Router()
 router.get('/register',UserController.register)
 router.get('/login',UserController.login)
 router.get('/logout',UserController.logout)
-
 router.post('/login',UserController.postlogin)
 router.post('/register',UserController.postregister)
-
-
-
 
 module.exports = router
