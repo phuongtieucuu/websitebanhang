18f7fc13f7a1f8d721a08ede5f52edde59ef21df
Thaihuy <hamo.thaihuy@gmail.com>
2022-08-22 08:25:43 +0700

    C00024

--- src/index.js
+++ src/index.js
@@ -3,41 +3,41 @@ const app = express()
 const port =  3000
 const handlebars = require('express-handlebars')
 const path = require('path')
-// const router = require('./routes/index')
+const router = require('./routes/index')
 const mongoose = require('mongoose')
-// const methodOverride = require('method-override')
-// var morgan = require('morgan')
-// // const bodyParser = require('body-parser')
-// var hbs = handlebars.create({})
-// const expressValidator = require('express-validator')
-// const cookieParser = require('cookie-parser')
-// const session = require('express-session');
+const methodOverride = require('method-override')
+var morgan = require('morgan')
+// const bodyParser = require('body-parser')
+var hbs = handlebars.create({})
+const expressValidator = require('express-validator')
+const cookieParser = require('cookie-parser')
+const session = require('express-session');
 const Category = require('./app/models/category')
-// const fileUpload = require('express-fileupload')
+const fileUpload = require('express-fileupload')
 const {ObtoOb,ArtoOb} = require('./until/mongooes')
-// const passport = require('passport')
-// const {check,message} = require('./app/middleware/client')
+const passport = require('passport')
+const {check,message} = require('./app/middleware/client')
 mongoose
 .connect('mongodb+srv://websitebanhang:huy12345@websitebanhang.73phjjw.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('MongoDB Connected....'))
 .catch((err) => console.log(err))
-// require('./until/hbs')(hbs)
+require('./until/hbs')(hbs)
 // app.use(morgan('combined'))
-// app.use(methodOverride('_method'))
+app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
-// // app.use(bodyParser.json());
-// app.use(express.urlencoded({extended: true}));
-// app.use(expressValidator());
-// app.use(cookieParser());
-// app.use(fileUpload())
-// app.use(session({secret: 'mk', saveUninitialized: true, resave: false,cookie: { secure: true }}))
+// app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
+app.use(expressValidator());
+app.use(cookieParser());
+app.use(fileUpload())
+app.use(session({secret: 'mk', saveUninitialized: true, resave: false,cookie: { secure: false }}))
 app.engine('.hbs', handlebars.engine({extname: '.hbs'}))
 app.set('view engine', '.hbs');
 app.set('views', './src/resources/views')
-// app.use(passport.initialize())
-// app.use(passport.session())
-// require('./until/passport')(passport)
-// app.use(message)
+app.use(passport.initialize())
+app.use(passport.session())
+require('./until/passport')(passport)
+app.use(message)
 Category.find({},(err,data)=>{
   if(err){
     return console.log(err)
@@ -45,13 +45,13 @@ Category.find({},(err,data)=>{
     app.locals.category = ArtoOb(data)
   }
 })
-const productRouter = require('./routes/product')
-const producttypeRouter = require('./routes/producttype')
-const categoryRouter = require('./routes/category')
-const orderRouter = require('./routes/order')
-const cartRouter = require('./routes/cart')
-const userRouter = require('./routes/user')
-const siteRouter = require('./routes/site')
+// const productRouter = require('./routes/product')
+// const producttypeRouter = require('./routes/producttype')
+// const categoryRouter = require('./routes/category')
+// const orderRouter = require('./routes/order')
+// const cartRouter = require('./routes/cart')
+// const userRouter = require('./routes/user')
+// const siteRouter = require('./routes/site')
 
 // app.use('/admin/product',productRouter)
 // app.use('/admin/producttype',producttypeRouter)
@@ -60,10 +60,8 @@ const siteRouter = require('./routes/site')
 // app.use('/cart',cartRouter)
 // app.use('/user',userRouter)
 // app.use('/',siteRouter)
-app.get('/',(req,res) => {
-  res.render('home')
-})
-// router(app)
+
+router(app)
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}`)
